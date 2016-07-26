de297bb32231e34edba914dae970980278a96b99
Charles Pletcher <cpletcher2@gmail.com>
2016-07-26 15:01:04 -0400

    Update tests, remove sinon

--- test/index-test.js
+++ test/index-test.js
@@ -1,13 +1,13 @@
 /*global concatenatedString, describe, interpolatedString, it, myString */
 
 it('defines `greeting`', function() {
-  assert.equal(greeting, "Hello, everybody!")
+  expect(greeting).toEqual("Hello, everybody!")
 })
 
 it('concatenates strings to greet a special guest in `greetSpecialGuest`', function() {
-  assert.equal(greetSpecialGuest, "Hello, " + specialGuest + "!")
+  expect(greetSpecialGuest).toEqual(`Hello, ${specialGuest}!`)
 })
 
 it('interpolates a string in `conversation`', function() {
-  assert.equal(conversation, `Let's talk about ${topic}.`)
+  expect(conversation).toEqual(`Let's talk about ${topic}.`)
 })
