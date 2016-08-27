4bd18b2c394f327d9b05105e6a53a0592d2fdf1f
Anthony Zeitlin <zeitlin1@gmail.com>
2016-08-27 14:42:43 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -4,10 +4,16 @@ it('defines `greeting`', function() {
   expect(greeting).toEqual("Hello, everybody!")
 })
 
+/* greeting goes here */
+
 it('concatenates strings to greet a special guest in `greetSpecialGuest`', function() {
   expect(greetSpecialGuest).toEqual(`Hello, ${specialGuest}!`)
 })
 
+/* welcome goes here */
+
 it('interpolates a string in `conversation`', function() {
   expect(conversation).toEqual(`Let's talk about ${topic}.`)
 })
+
+/* convo starts here */
