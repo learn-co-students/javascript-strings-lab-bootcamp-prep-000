e469f16b569a84e00397ffad81a0f7cc4a660dd0
jw2340 <jin.wang.527@gmail.com>
2016-09-03 18:48:29 +0900

    Update index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+var greeting = "Hello, everybody!";
+
+var specialGuest = "Neil deGrasse Tyson"
+
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
+
+var topic = "space";
+
+var conversation = `Let\'s talk about ${topic}.`;
