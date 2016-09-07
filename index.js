d6f146a42536b4ab0d6c068db306c33adde25fce
Tiara  Kaimal <tiarakaimal@gmail.com>
2016-09-07 03:38:10 +0000

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ var greetSpecialGuest = "" + specialGuest + "!";
 var topic = "space";
 
 var conversation = `${topic}`;
+
+var greeting = "Hello, everybody!";
+
+var greetSpecialGuest = "Hello, Neil deGrasse Tyson!";
+
+var conversation = "Let's talk about space."
