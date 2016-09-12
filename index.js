9ba8b121062bd8dc0e32198cdb955d031cc7d9b6
Frank Faustino <nawzyah@gmail.com>
2016-09-12 19:56:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-learvar greeting = "Hello, everybody!";
+var greeting = "Hello, everybody!";
 
-var specialGuest = "Neil deGrasse Tyson";
+var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `Let\'s talk about ${topic}.`;
+var conversation = `Let's talk about ${topic}.`;
