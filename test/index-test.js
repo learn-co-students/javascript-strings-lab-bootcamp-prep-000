04d58cd86fa3356da9b4b0e695253b043c2db5fc
Daniel Kamara <danielkamara1983@gmail.com>
2016-10-05 01:28:25 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,8 +1,7 @@
 /*global concatenatedString, describe, interpolatedString, it, myString */
 
-it('defines `greeting`', function() {
-  expect(greeting).toEqual("Hello, everybody!")
-})
+var greeting = "Hello everybody!";
+
 
 it('concatenates strings to greet a special guest in `greetSpecialGuest`', function() {
   expect(greetSpecialGuest).toEqual(`Hello, ${specialGuest}!`)
