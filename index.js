d86c853d88a264f1e5c6a7805462ebd5a8d10834
Chenxi Wu <wuchenxi2013@gmail.com>
2016-10-14 02:17:49 +0000

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,6 @@ var greetSpecialGuest = "" + specialGuest + "!";
 var topic = "space";
 
 var conversation = `${topic}`;
+greeting='Hello, everybody!'
+greetSpecialGuest='Hello, '+specialGuest+'!'
+conversation=`Let\'s talk about ${topic}.`
