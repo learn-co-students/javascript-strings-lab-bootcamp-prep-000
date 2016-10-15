55eed1cad15a27ca1c59adca680d985b03c108af
haroon khan <khanshahar85@gmail.com>
2016-10-15 01:38:17 +0000

    Done.

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "welcome" + specialGuest + "your the man!";
+var greetSpecialGuest = "Hello" + specialGuest;
 
-var topic = "space";
+var topic = "space!";
 
-var conversation = `${topic}`;
+var conversation = `let's talk about ${topic}`;
