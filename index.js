23ad19ccfcc00f69f9bb6405924690a57167653e
KWAME EDUSAH <kwame.edusah@gmail.com>
2016-10-17 00:03:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
@@ -6,4 +6,4 @@ var greetSpecialGuest = "" + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `${topic}`;
+var conversation = "Let's talk about " + {topic}.
