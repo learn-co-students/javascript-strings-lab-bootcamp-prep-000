37e84e53933ee62dcea1a275ddef9ff0ebe5bbaa
Alex Moreno <amorenop22@hotmail.com>
2016-10-21 06:40:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var greeting = "Hello, everybody!";
 
-var specialGuest = "Neil deGrasse Tyson"
+var specialGuest = "Neil deGrasse Tyson";
 
-var greetSpecialGuest = "Hello" + specialGuest + "!";
+var greetSpecialGuest = "hello" + specialGuest + "!";
 
 var topic = "let\'s talk about space";
 
