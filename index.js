ee7b2dcd2ecd419f6c07643f7ae6ffb40c560616
bilal Hamada <bilal.k.hamada@gmail.com>
2016-10-29 15:22:04 +0000

    Done.

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var greeting = "";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "space";
+var topic = "";
 
-var conversation = `${topic}`;
+var conversation = "Let's talk about .";
+var greeting = "Hello, everybody!";
