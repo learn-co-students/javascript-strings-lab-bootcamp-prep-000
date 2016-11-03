d883bf1d1ad41f126ed8f1e39d25ea5db356a3be
Naarai Hernandez <hernnara@gmail.com>
2016-11-03 06:27:24 +0000

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var greeting = "Hello, everybody!"; // "!" was not incuded
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = `Hello, ${specialGuest}!`;
+var greetSpecialGuest = "Hello, " + specialGuest + "!"; 
+// var greetSpecialGuest = `Hello, ${specialGuest}!`;
 
 var topic = "space";
 
