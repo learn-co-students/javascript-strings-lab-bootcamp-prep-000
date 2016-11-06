b29e2ff8e8a0654f9cc293012864cd04cf371533
Jack Lord <lord.jack@gmail.com>
2016-11-06 23:00:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
-var specialGuest = "Neil deGrasse Tyson"
+var specialGuest = "Isaac Newton"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "space";
+var topic = "gravity";
 
-var conversation = `${topic}`;
+var conversation = `Let\'s talk about ${topic}.`;
