831423d7ce0524fa051f4bb15e561ba6091b5034
Ursh Khan <urshkhan@gmail.com>
2016-12-21 23:38:00 +0000

    Done.

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ var specialGuest = "Neil deGrasse Tyson"
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
-var space = 'Let\'s talk about space.'
-var conversation = `${space}`;
+
+var conversation = `Let's talk about ${topic}.`;
