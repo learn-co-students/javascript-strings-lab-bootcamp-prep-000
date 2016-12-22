55426bf6bfdb74f8e36d8a21b1111a2ccab1c0f9
IKSHVAKU SINGAPURAM <ixs21870@ucmo.edu>
2016-12-22 01:38:58 +0000

    Done.

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "Let's talk about space";
+var topic = "space";
 
 var conversation = `${topic}`;
