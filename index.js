38e7c9bfc95abde4d9d1d1b646f75612302fd6ed
Vivian Kulumba <vkulumba@gmail.com>
2016-12-22 00:35:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hello," + "Neil deGrasse Tyson" + "!";
 
 var topic = "space";
 
-var conversation = `${topic}`;
+var conversation = ${Lets talk about Space}
