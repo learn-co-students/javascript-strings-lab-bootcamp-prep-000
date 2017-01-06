c6127d21b823c43750d02c1e1f8df5512c219a27
devin hanaway <devinh11@gmail.com>
2017-01-06 21:23:08 +0000

    Done.

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "Let's talk about";
+var topic = "space";
 
-var conversation = topic + " " + topic + "." ;
+var conversation = `Let's talk about ${topic}.`;
