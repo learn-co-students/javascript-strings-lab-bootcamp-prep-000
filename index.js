e04bbd6429dcc846a46ff475cbdea77877c569aa
Crystal Scott <crystalscott216@gmail.com>
2017-01-09 15:59:18 +0000

    Done.

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var greeting = "Hello, everybody!"
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + "${specialGuest}" + "!"
+var greetSpecialGuest = `Hello, ${specialGuest}!`
 
-var topic = "${space}";
 
-var conversation = `Lets talk about ${space}.`;
+var topic = "space";
+
+var conversation = `Let\'s talk about ${topic}.`;
