bb22806965afcf67d1a3b5022be4cf21e6686a77
Jennifer Vargas <jennvarg90@outlook.com>
2017-01-11 22:30:51 +0000

    Done.

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!"; // "Hello, Neil deGrasse
 
 var topic = "Let's talk about space.";
 
-var conversation = `Let's talk about ${topic}.`
+var conversation = `Let's talk about ${topic}.` // "Let's talk about let's talk about space."
