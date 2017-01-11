1b7b6159158f3d47bfb1bbfe19fa968e3987b255
Ghali Mazzouri <gm495@scarletmail.rutgers.edu>
2017-01-11 06:50:44 +0000

    Done.

--- index.js
+++ index.js
@@ -4,6 +4,5 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "Let's talk about space";
-
-var conversation = `${topic}`;
+var topic = "space";
+var conversation = "Let's talk about " + `${topic}`+ ".";
