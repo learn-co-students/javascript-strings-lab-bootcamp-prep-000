577ce0287158a1b7d5d27db593738eead502c2dc
Juha  Bach <cdalsoniii@gmail.com>
2017-01-13 11:46:47 +0000

    Done.

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = greeting.substring(0,5) + ", " + specialGuest + "!";
 
 var topic = "space";
 
 var conversation = `${topic}`;
+
+conversation = "Let\'s talk about " + topic + ".";
