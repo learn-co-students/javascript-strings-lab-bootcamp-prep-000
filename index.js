97382fe2522108ef93264cc6f184c79a6df75029
Ahna Hughes <ahnahughes@gmail.com>
2017-01-14 01:37:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
-var specialGuest = "Neil deGrasse Tyson"
+var specialGuest = "Neil deGrasse Tyson" "hello"+specialGuest+"!"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "hello" + specialGuest + "!";
 
-var topic = "space";
+var topic = "space"; 'let's talk about {space}'
 
-var conversation = `${topic}`;
+var conversation = `${}`;
