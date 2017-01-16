622e066b181fd7dcbbb27c4afe559b3cf0531b6f
Khoa Ma <khoa.h.ma@gmail.com>
2017-01-16 16:48:37 +0000

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "Hello," + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
