568379484f66ba8593549f54a7152bcd6cbab775
David Regan <dregan1@comcast.net>
2017-01-24 18:42:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var greeting = "";
+var greeting = 'Hello, World!''
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hello" + specialGuest + "!";
 
-var topic = "space";
+var topic = "Bill Nye";
 
-var conversation = `${topic}`;
+var conversation = `Let's talk about${topic}`;
