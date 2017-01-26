357ab774d831f0bc49e9fc7338484ceefdaf09b1
Jesse Stern <jessesternmusic@gmail.com>
2017-01-26 05:58:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
