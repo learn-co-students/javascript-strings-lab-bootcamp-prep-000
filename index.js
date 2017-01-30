3ad2c0b26c4773c90d7cfab210d77b3b825a0174
Rayden Cheung <chinaguy@gmail.com>
2017-01-30 10:04:56 +0000

    Done.

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "space";
+var topic = "space.";
 
 var conversation = "Let's talk about " + `${topic}`;
