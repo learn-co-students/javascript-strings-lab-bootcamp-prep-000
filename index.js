75b56bf631a9cc1f68e36a967f027d3bdbf078d6
Zack Enders <zenders672@gmail.com>
2017-02-05 16:17:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
 var conversation = `${topic}`;
+
+conversation = `Let\'s talk about ${topic}.`
