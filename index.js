c94ad0f597736ada1e9e10b52b6834e3ab04629d
Sarah Vittetoe <sarah.vittetoe@gmail.com>
2017-02-14 01:27:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-var greeting = "";
+var greeting = "Hello,";
+greeting + "everybody" + "!"
 
 var specialGuest = "Neil deGrasse Tyson"
+greeting + specialGuest + "!"
 
 var greetSpecialGuest = "" + specialGuest + "!";
 
