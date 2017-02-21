f3df1df87922a130f450cc3b67bfbdf1a3a7bd32
Aneus Sangster <sangsteraneus@yahoo.com>
2017-02-21 02:08:49 +0000

    Done.

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "greeting" + specialGuest + "topic";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `${Let\'s talk about space.}`;
+var conversation = `Let's talk about ${topic}.`;
