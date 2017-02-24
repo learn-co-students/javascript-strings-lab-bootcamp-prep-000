ad7b17d0eb9c55fccf6b2eb34e8866b7128498d9
Matthew Croak <mattcroak718@gmail.com>
2017-02-24 21:44:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-var greeting = "";
+var greeting = "Hello, everybody!"
+var specialGuest = "Neil deGrasse Tyson";
 
-var specialGuest = "Neil deGrasse Tyson"
-
-var greetSpecialGuest = "" + specialGuest + "!";
-
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
+earn
 var topic = "space";
 
-var conversation = `${topic}`;
+var conversation = `Let's talk about  ${topic}.`;
