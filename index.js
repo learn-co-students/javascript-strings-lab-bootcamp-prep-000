a0982313d3da2766fcb06822931d7c2c92ec4997
Krasimir Vatchinsky <kvatchinsky@icloud.com>
2017-03-01 22:09:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
-var specialGuest = "Neil deGrasse Tyson"
+var specialGuest = "Neil deGrasse Tyson";
+"Hello, " + specialGuest + "!";
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `${topic}`;
+var conversation = `Let\'s talk about ${topic}.`;
