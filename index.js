8bc2e2958d32d10870309ab7638015bb837aadbf
Ana Edwards <cenicienta58@gmail.com>
2017-03-02 17:10:51 +0000

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = 'Hello, everybody!';
 
 var specialGuest = "Neil deGrasse Tyson!"
 
-var greetSpecialGuest = "" +lorena+ "!";
+var greetSpecialGuest = "" + lorena + "!";
 
 var topic = "space";
 
