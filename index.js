f0cfcc236b6b75b269c9477469073a1870cf6c98
Haejin Kim <haejinkim88@gmail.com>
2017-03-09 02:07:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var greeting = "Hello Everybody!";
+var greeting = "Hello, everybody!";
 
-var specialGuest = "Neil deGrasse Tyson"
+var specialGuest ="Neil deGrasse Tyson"
 
 var greetSpecialGuest = "" + specialGuest + "!";
 
-var topic = "space";
+var topic = "space.";
 
 var conversation = `${topic}`;
