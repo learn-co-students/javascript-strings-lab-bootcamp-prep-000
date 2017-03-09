753a129d45c7ef1ee7c41efb2dfa559b7ce77546
Sherri Ogunlowo <smcatlin_1@yahoo.com>
2017-03-09 03:20:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Welcome!" + specialGuest + "Thank you for coming!";
 
-var topic = "space";
+var topic = "space"
 
-var conversation = `${topic}`;
+var conversation = `${topic}`;
