07ca3b039d3c5e8878102d5c14c07e8de60d3ed0
yesharge <yeshargeabrha@gmail.com>
2017-03-13 13:41:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+// assigning variable greeting to string
 var greeting = "Hello, everybody!";
-
+// assigning variable specialGuest to string
 var specialGuest = "Neil deGrasse Tyson"
-
+// Concatenating strings 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
-
+// inserting placeholder into string
 var conversation = `Let's talk about ${topic}.`;
