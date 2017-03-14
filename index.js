31c59d5c1da3971f8523e280ce62e3358c74fb0f
Ana Vasquez <anvasquez08@gmail.com>
2017-03-14 01:37:28 +0000

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "Hello, "  + specialGuest + "!";
+var greetSpecialGuest = greeting + specialGuest + "!";
 
 var topic = "Let's talk about space";
 
