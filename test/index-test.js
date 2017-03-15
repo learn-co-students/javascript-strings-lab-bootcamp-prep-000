c3356957c8c0b03ea1a931685955c5e7a52de9bc
Juan Bautista Daudinòt Gonzalez <jackdaudinotny@gmail.com>
2017-03-15 23:50:35 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -10,8 +10,8 @@ it('concatenates strings to greet a special guest in `greetSpecialGuest`', funct
 
 it('interpolates a string in `conversation`', function() {
   expect(conversation).toEqual(`Let's talk about ${topic}.`)
-})var greeting = "Hello, Neil deGrasse Tyson!";
+})var greeting = "Hello, everybody!";
 var specialGuest = "Neil deGrasse Tyson"
-var greetSpecialGuest = "" + specialGuest + "!";
-var topic = "space";
-var conversation = `${topic}`;
+var greetSpecialGuest = "hello, " + specialGuest + "!";
+var topic = "drinking at home";
+var conversation = `Would you like to have a drink, ${specialGuest}!`
