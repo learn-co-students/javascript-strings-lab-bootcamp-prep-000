333e6b30b9d4e67e44de47f0696bcc18097df0ab
Lindi Smith <lindi.strang@gmail.com>
2017-03-18 14:28:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var greeting = "";
-
+var greeting = "Hello, everybody!";
+  		  
 var specialGuest = "Neil deGrasse Tyson"
-
-var greetSpecialGuest = "" + specialGuest + "!";
-
-var topic = "space";
-
-var conversation = `${topic}`;
+  		  
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
+  		  
+var topic = "space" ;
+  		  
+var conversation = "Let's talk about " + topic + ".";
