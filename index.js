7f6569a4473279e4831275824b8fc016cd6862b4
Carlos Ortega Morales <carlos.ortega86@gmail.com>
2017-03-22 17:02:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "space";
+var conversation = "Let\'s talk about space.";
 
-var conversation = `${topic}`;
+var topic = "space"
+
+var space = `${topic}`;
