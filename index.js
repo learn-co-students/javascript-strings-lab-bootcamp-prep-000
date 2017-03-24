ca57912e39df1fd265a288befe6f164cd7acbf94
Katrena Haney <katrenahaney@gmail.com>
2017-03-24 02:15:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var greeting = "";
-
+var greeting = 'Hello, everybody!';
 var specialGuest = "Neil deGrasse Tyson"
-
+var specialGuest = "Neil deGrasse Tyson";
 var greetSpecialGuest = "" + specialGuest + "!";
-
+var greetSpecialGuest = "Hello, everybody!', + "Neil deGrasse Tyson";
 var topic = "space";
-
+var topic = 'Let's talk about space.";
 var conversation = `${topic}`;
+var conversation = ${3+5};
