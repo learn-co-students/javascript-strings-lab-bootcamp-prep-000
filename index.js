96f3fbd6c95e983c5a80282b0d429a6f5fb9517c
John Petsagourakis <jpets91@gmail.com>
2017-03-26 16:18:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var greeting = "Hello, everybody!";
+//stores the string hello everybody in the var "greeting"
 
 var specialGuest = "Neil deGrasse Tyson"
+//does the same thing as above
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
+//stores a string plus a previous var into a new var
 
 var topic = "space";
 
 var conversation = `Let\'s talk about ${topic}.`;
+//escapes the ' with a backslash, interpolates the variable inside a string so that it runs
