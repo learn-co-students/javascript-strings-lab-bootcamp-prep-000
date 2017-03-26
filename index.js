2ab4cee7e4c490367d0f4eed27309737ba4f8e52
Saif Mahmud <smahmud46@yahoo.com>
2017-03-26 03:18:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var greeting = "";
+var greeting = "Hi";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = greeting + "," + " " + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `${topic}`;
+var conversation = "Let\'s talk about" + " " + `${topic}` + ".";
