09f2d23f9a3c3347821850e729d6551292acc5a0
steven Miserendino <steven.miserendino@gmail.com>
2017-03-28 16:07:28 +0000

    Done.

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "Let's talk about space";
+var topic = " Let's talk about space ";
 
 var conversation = `Let's talk about ${topic}.`;
