b2e038f2dfcadf946c8fadba0c1991590e862c07
Andrea Szocs <andreea.szocs@gmail.com>
2017-03-30 15:43:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
+var conversation = `Let\'s talk about ${topic}.`;
+
+/*
+
+var greeting = "Hello, everybody!";
+
+var specialGuest = "Hello,Neil deGrasse Tyson"
+
+var greetSpecialGuest = "" + specialGuest + "!";
+
+var topic = "Let\'s talk about space";
+
 var conversation = `${topic}`;
+
+*/
