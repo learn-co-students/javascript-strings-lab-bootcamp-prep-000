7d572af811397e24958afe52aca7e0c8ee06abfa
Danielle Lewan <d_lewan@aol.com>
2017-04-01 05:48:07 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -3,11 +3,15 @@
 it('defines `greeting`', function() {
   expect(greeting).toEqual("Hello, everybody!")
 })
+var greeting = `Hello, everybody!`
 
 it('concatenates strings to greet a special guest in `greetSpecialGuest`', function() {
   expect(greetSpecialGuest).toEqual(`Hello, ${specialGuest}!`)
 })
+var greetSpecialGuest = `Hello, Neil deGrasse Tyson!`
 
 it('interpolates a string in `conversation`', function() {
   expect(conversation).toEqual(`Let's talk about ${topic}.`)
 })
+conversation = 'lets talk about "space".'
+var conversation = "Let's talk about space."
