a6ad48e14b24284502b53308ce48ed2a46a6f83f
James Bland <james.kyle.bland@gmail.com>
2017-04-03 19:42:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
-var specialGuest = "Neil deGrasse Tyson"
+var specialGuest = "Hello, Neil deGrasse Tyson"
 
 var greetSpecialGuest = "" + specialGuest + "!";
 
-var topic = "space";
+var topic = "Let\'s talk about space.";
 
 var conversation = `${topic}`;
