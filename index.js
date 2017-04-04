5779f687ca1054cca7c75edd6b0d24b33978c1b9
margaretta hemmings <mbcc1905@hotmail.com>
2017-04-04 22:03:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = `Hello, ${specialGuest}!`;
 
 var topic = "space";
 
-var conversation = `${topic}`;
+var conversation = `Let's talk about ${topic}.`;
+
+console.log(conversation)
+console.log(greetSpecialGuest)
