2d0bf48894d724fe217a5464297cf15399123561
Cesar  Serrano <serrano598@gmail.com>
2017-04-04 08:27:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `${topic}`;
+var conversation = `Let\'s talk about ${topic}`;
+
+var Lets talk about space = `Let\'s talk about ${topic}`;
