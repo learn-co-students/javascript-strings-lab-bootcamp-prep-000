e5bb7e721c8094e6c5b1ff00e9432356867bd4bb
Leon Malisov <leonmalisov@gmail.com>
2017-04-06 23:59:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `${topic}`;
+var conversation = `Let's talk about ${topic}.`;
+
+var math = 2+2;
+
+var mathFun = `In fact, ${conversation} b${math} lunch.`;
