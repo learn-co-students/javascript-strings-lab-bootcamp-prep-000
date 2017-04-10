fbe77a0c993cdc73be01f4f7e9fe34f1ad7c870b
Daphne Domalis <damolisi@gmail.com>
2017-04-10 21:33:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var greeting = "";
+var greeting = "Hello";
 
 var specialGuest = "Neil deGrasse Tyson"
 
@@ -6,4 +6,4 @@ var greetSpecialGuest = "" + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `${topic}`;
+var conversation = `${topic};
