e7ab974ecb8d86086426828c1b42c5bae505a999
Kelvin Minaya <k.minaya@aol.com>
2017-04-12 22:44:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
-var specialGuest = "Neil deGrasse Tyson"
+var specialGuest = "Neil deGrasse Tyson!";
 
-var greetSpecialGuest = "" + specialGuest + "!";
 
-var topic = "space";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var conversation = `${topic}`;
+
+var topic = "let'\s talk about space";
+
+
+var conversation = `Let'\s talk about ${topic}.`;
